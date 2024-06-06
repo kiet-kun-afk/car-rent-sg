fc9df3f27a39f712e379dc7cdf9e8a6c634449ec
kiet-kun-afk <ilovevy19@gmail.com>
2024-06-06 21:18:54 +0700

    fox

--- backend/src/main/java/app/repository/CitizenCardRepository.java
+++ backend/src/main/java/app/repository/CitizenCardRepository.java
@@ -8,23 +8,4 @@ import app.model.cards.CitizenCard;
 
 public interface CitizenCardRepository extends JpaRepository<CitizenCard, Integer> {
 
-	boolean existsByIdCard(Integer idCard);
-
-	CitizenCard findByIdCard(Integer idCard);
-
-	@Query("""
-			SELECT c FROM CitizenCard c
-			JOIN Customer cu
-			ON c.citizenId = cu.citizenCard.citizenId
-			WHERE cu.customerId = :customerId
-				""")
-	CitizenCard findByCustomerId(@Param("customerId") Integer customerId);
-
-	@Query("""
-			SELECT c FROM CitizenCard c
-			JOIN Staff s
-			ON c.citizenId = s.citizenCard.citizenId
-			WHERE s.staffId = :staffId
-				""")
-	CitizenCard findByStaffId(@Param("staffId") Integer staffId);
 }
