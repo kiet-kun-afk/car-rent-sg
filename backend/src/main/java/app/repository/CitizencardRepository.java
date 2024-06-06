8c294ff6bf9fd16e15e695d571a3f2fbf8caa6cc
kiet-kun-afk <ilovevy19@gmail.com>
2024-06-06 21:30:51 +0700

    fakkakak

--- backend/src/main/java/app/repository/CitizencardRepository.java
+++ backend/src/main/java/app/repository/CitizencardRepository.java
@@ -1,6 +1,8 @@
 package app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import app.model.cards.CitizenCard;
 
