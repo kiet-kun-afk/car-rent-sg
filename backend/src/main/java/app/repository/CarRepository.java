6f591c64b2d909dcaaf862f193ee25e5d2d32c31
kiet-kun-afk <ilovevy19@gmail.com>
2024-06-12 00:17:19 +0700

    cars for index

--- backend/src/main/java/app/repository/CarRepository.java
+++ backend/src/main/java/app/repository/CarRepository.java
@@ -1,12 +1,35 @@
 package app.repository;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import app.model.Car;
 
-public interface CarRepository extends JpaRepository<Car, Integer> {
+public interface CarRepository extends JpaRepository<Car, Integer>, JpaSpecificationExecutor<Car> {
 
     Car findByRegistrationPlateAndStatusTrue(String registrationPlate);
 
     Car findByRegistrationPlate(String registrationPlate);
+
+    Page<Car> findAll(Specification<Car> spec, Pageable pageable);
+
+    @Query("""
+            SELECT c
+            FROM Car c
+            WHERE NOT EXISTS (
+                SELECT 1
+                FROM Contract ct
+                WHERE ct.car = c
+                AND :today BETWEEN ct.startDate AND ct.endDate
+            )
+            AND c.status = true
+            ORDER BY NEWID()
+            """)
+    Page<Car> findCarsNotInContractToday(LocalDateTime today, Pageable pageable);
 }
--- backend/src/main/java/app/service/CarService.java
+++ backend/src/main/java/app/service/CarService.java
@@ -2,6 +2,8 @@ package app.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import app.dto.CarDTO;
 
 import app.response.CarResponse;
@@ -19,5 +21,8 @@ public interface CarService {
 	public void Delete(String registrationPlate) throws Exception;
 
 	// public CarResponse DeletePut(Integer id, CarDTO carDTO);
+
+	public Page<CarResponse> getCarsForIndex(Integer pageNumber, Integer pageSize)
+			throws Exception;
 
 }
